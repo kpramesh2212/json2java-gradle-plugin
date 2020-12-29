ca53fb170e85e0db9a62ac0e420b4dbb2f7456f9
Ramesh KP <kpramesh2212@gmail.com>
2020-12-29 14:30:39 +0000

    Add contents for 1.0.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
+    id("net.researchgate.release") version "2.8.1"
+    id("com.gradle.plugin-publish") version "0.12.0"
 }
 
 
@@ -39,5 +41,26 @@ publishing {
             name = "local"
             url = uri("$buildDir/repo")
         }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/kpramesh2212/json2java-gradle-plugin"
+    vcsUrl = "https://github.com/kpramesh2212/json2java-gradle-plugin.git"
+    tags = listOf("json2java", "jsonschema2java", "json2pojo", "jsonschema2pojo", "yaml2java", "yaml2pojo", "pojo-generator")
+}
+
+val afterReleaseBuild by tasks.existing
+
+afterEvaluate {
+  afterReleaseBuild {
+      dependsOn(tasks.named("publish"))
+      dependsOn(tasks.named("publishPlugins"))
+  }
+}
+
+release {
+    (getProperty("git") as net.researchgate.release.GitAdapter.GitConfig).apply {
+        requireBranch = "main"
     }
 }
